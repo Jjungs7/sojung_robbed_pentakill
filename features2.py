27a771f2f3f399e39a26230d3736aadbf52a1d95
Youngju Jeon <yjjeon0219@gmail.com>
2020-10-07 17:22:17 +0900

    substraction edited

--- features2.py
+++ features2.py
@@ -3,8 +3,8 @@ def sub(a, b):
     # 첫 안내에는 "1st change" 부분을,
     # 두 번째 안내에는 "2nd change" 부분을 처리해주세요
 
-    return a + b  # comment this on 1st change
-    #return b - a  # uncomment this on 1st change
+    #return a + b  # comment this on 1st change
+    return b - a  # uncomment this on 1st change
                    # comment above line on 2nd change
     #return a - b  # uncomment this on 2nd change
 
